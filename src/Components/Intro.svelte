ef1c3d6e65f0ffdfadb8c8d618f866caf9c4b360
Jared Wilber <jdwlbr@gmail.com>
2024-05-24 21:45:06 -0700

    init repo

---
+++ src/Components/Intro.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<section>
+  <p class="body-text">
+    Lorem ipsum dolor sit amet <a href="">random link</a> adipisicing elit. Esse
+    dolor nobis
+    <span class="bold">bold text</span> eius voluptate distinctio odit ut quod illo!
+    Nulla consequatur fugiat quia commodi aspernatur necessitatibus est doloremque
+    quam vitae dolorem. Vitae natus ipsum animi officiis et quis obcaecati in molestiae
+    aperiam, perferendis odio a ipsam nam suscipit laboriosam corporis excepturi
+    quisquam? Ratione delectus repellat error maxime quo beatae ullam, quod
+  </p>
+  <p class="body-text">This is a trr</p>
+</section>
+
+<style>
+</style>
