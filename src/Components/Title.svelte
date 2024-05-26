a0b5e6329ed7334673984dbe04fd0af321f4a606
nade <i@nade.me>
2024-05-26 00:41:52 -0700

    update

--- src/Components/Title.svelte
+++ src/Components/Title.svelte
@@ -1,8 +1,8 @@
 <section id="intro">
-  <h1 id="intro-hed">Title Of The Article</h1>
-  <h1 class="intro-sub">Subtitle of the article.</h1>
+  <h1 id="intro-hed">Explained: Type I, Type II Errors and Power</h1>
+  <h1 class="intro-sub">How strong is your Null Hypothesis?</h1>
   <h3 id="intro__date">
-    <a href="https://twitter.com/" target="_blank">Author Name</a>, Month Year
+    Allen Zhang & Brendan Kuang, June 2024
   </h3>
 </section>
 
--- src/Meta.svelte
+++ src/Meta.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
-  <title>MLU-Explain:</title>
+  <title>Explained: Type I, Type II Errors and Power</title>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width,initial-scale=1" />
@@ -8,15 +8,15 @@
     content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
   />
   <!-- add desc -->
-  <meta name="description" content="Svelte Interactive Article Starter" />
+  <meta name="description" content="An interactive explanation of Type I, Type II Errors and Power in Statistics" />
   <!-- add image -->
   <meta property="og:image" content="" />
   <!-- add title -->
-  <meta property="og:title" content="Svelte Interactive Article Starter" />
+  <meta property="og:title" content="Explained: Type I, Type II Errors and Power" />
   <!-- add prop -->
   <meta
     property="og:description"
-    content="Svelte Interactive Article Starter"
+    content="An interactive explanation of Type I, Type II Errors and Power in Statistics"
   />
   <meta property="og:image:width" content="1000" />
   <meta property="og:image:height" content="630" />
